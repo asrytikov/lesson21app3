d7f0953332ac1736a3fb5ba41bee5e3a6787c30d
asrytikov <asrytikov@gmail.com>
2022-11-27 11:57:17 +0400

    Finish app

--- src/main/java/com/example/lesson21app3/controllers/ProductController.java
+++ src/main/java/com/example/lesson21app3/controllers/ProductController.java
@@ -1,9 +1,10 @@
 package com.example.lesson21app3.controllers;
 
+import com.example.lesson21app3.model.Product;
 import com.example.lesson21app3.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProductController {
@@ -14,8 +15,22 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @RequestMapping("/products")
+    //@RequestMapping("/products")
+    @GetMapping("/products")
     public String viewProducts(Model model){
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+        return "products.html";
+    }
+
+    //@RequestMapping(path="/products", method = RequestMethod.POST)
+    @PostMapping("/products")
+    public String addProduct(/*@RequestParam String name, @RequestParam Double price,*/ Product p, Model model){
+        /*Product p = new Product();
+        p.setName(name);
+        p.setPrice(price);*/
+        productService.addProduct(p);
+
         var products = productService.findAll();
         model.addAttribute("products", products);
         return "products.html";
